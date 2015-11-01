53c6cd16ebbcdf9fd9bc84b89cb25e1c36b4c79a
Andrew Ettenger <ettenger@gmail.com>
2015-11-01 14:33:25 -0500

    Load new plant record from Firebase on plant selector change

--- app/input/input.js
+++ app/input/input.js
@@ -4,24 +4,35 @@ angular.module('myApp.input', ['ngRoute'])
 .controller('InputController', ['$scope', '$firebaseObject', function($scope, $firebaseObject) {
 	var ref = new Firebase("https://ppr-field-data.firebaseio.com/")
 
+	// Default plant info
 	$scope.plantInfo = {
 		site: 'Haddington',
 		expt: 'SOSP',
 		id: '001',
+		species: ''
 	};
 
-	var plantRecord = $firebaseObject(ref
-		.child($scope.plantInfo.site)
-		.child($scope.plantInfo.expt)
-		.child('plants')
-		.child($scope.plantInfo.id)
-		.child('plantInfo')
-	);
+	// Update the plant info when Plant Selector fields change
+	$scope.updatePlantInfo = function() {
+		// load in record from Firebase
+		var plantRecord = $firebaseObject(ref
+			.child($scope.plantInfo.site)
+			.child($scope.plantInfo.expt)
+			.child('plants')
+			.child($scope.plantInfo.id)
+			.child('plantInfo')
+		);
 
-	plantRecord.$loaded().then(function() {
-		$scope.plantInfo.species = plantRecord.species;
-	});
+		// when loaded, get species
+		plantRecord.$loaded().then(function() {
+			$scope.plantInfo.species = plantRecord.species;
+		});
+	};
 
+	// Get the plant info for the default plant
+	$scope.updatePlantInfo();
+
+	// Default observation info
 	$scope.observation = {
 		height: '',
 		caliper: '',
