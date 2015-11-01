aa7ec3cfab7cb183efe9ea26be983986934fa20e
Andrew Ettenger <ettenger@gmail.com>
2015-11-01 14:19:12 -0500

    Wire up to Firebase to retrieve plant info

--- app/input/input.js
+++ app/input/input.js
@@ -1,13 +1,26 @@
 'use strict';
 
 angular.module('myApp.input', ['ngRoute'])
-.controller('InputController', ['$scope', function($scope) {
+.controller('InputController', ['$scope', '$firebaseObject', function($scope, $firebaseObject) {
+	var ref = new Firebase("https://ppr-field-data.firebaseio.com/")
+
 	$scope.plantInfo = {
 		site: 'Haddington',
 		expt: 'SOSP',
 		id: '001',
-		species: 'Cornus florida'
 	};
+
+	var plantRecord = $firebaseObject(ref
+		.child($scope.plantInfo.site)
+		.child($scope.plantInfo.expt)
+		.child('plants')
+		.child($scope.plantInfo.id)
+		.child('plantInfo')
+	);
+
+	plantRecord.$loaded().then(function() {
+		$scope.plantInfo.species = plantRecord.species;
+	});
 
 	$scope.observation = {
 		height: '',
